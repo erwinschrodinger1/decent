343b3e365d40ecda216661bbc294b2832e17c83f
erwinschrodinger1 <sunnynirjal@gmail.com>
2024-05-12 00:25:39 +0545

    Logging for wrokign checkout

--- src/components/background/scripts/index.js
+++ src/components/background/scripts/index.js
@@ -1,79 +1,68 @@
+// const isMetaMaskInstalled = () => {
+//   return Boolean(window.ethereum && window.ethereum.isMetaMask);
+// };
 
-const isMetaMaskInstalled = () => {
-  return Boolean(window.ethereum && window.ethereum.isMetaMask);
-};
+// const connectToMetaMask = async () => {
+//   if (!isMetaMaskInstalled()) {
+//     console.error('MetaMask is not installed');
+//     return;
+//   }
 
-const connectToMetaMask = async () => {
-  if (!isMetaMaskInstalled()) {
-    console.error('MetaMask is not installed');
-    return;
-  }
+//   try {
+//     const accounts = await window.ethereum.request({
+//       method: 'eth_requestAccounts',
+//     });
+//     console.log('Connected accounts:', accounts);
+//     return accounts;
 
-  try {
-    const accounts = await window.ethereum.request({
-      method: 'eth_requestAccounts',
-    });
-    console.log('Connected accounts:', accounts);
-    return accounts;
+//   } catch (error) {
+//     console.error('Error connecting to MetaMask:', error);
+//     return [];
+//   }
+// };
 
-  } catch (error) {
-    console.error('Error connecting to MetaMask:', error);
-    return [];
-  }
-};
+// connectToMetaMask();
 
-connectToMetaMask();
+// const handleSignIn = async () => {
+//   if(!isMetaMaskInstalled()) {
+//     console.error('MetaMask is not installed');
+//     return;
+//   }
+//   else {
+//     const accounts = await connectToMetaMask();
+//     if(accounts.length > 0) {
+//       const account = accounts[0];
+//       const response = await fetch('', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({account}),
+//       });
+//       if(response.ok) {
+//         console.log('User signed in');
+//         const data = await response.json();
+//         handleAuthentification(data)
+//         console.log(data);
+//       }
 
+//     }
 
-const handleSignIn = async () => {
-  if(!isMetaMaskInstalled()) {
-    console.error('MetaMask is not installed');
-    return;
-  }
-  else {
-    const accounts = await connectToMetaMask();
-    if(accounts.length > 0) {
-      const account = accounts[0];
-      const response = await fetch('', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({account}),
-      });
-      if(response.ok) {
-        console.log('User signed in');
-        const data = await response.json();
-        handleAuthentification(data)
-        console.log(data);
-      }
+//     else {
+//       console.error('User is not signed in');
+//     }
 
+//   }
+// }
 
-    }
+// const handleAuthentification = async () => {
+//   const{token} = data;components
+//   localStorage.setItem('token', token);
 
-    else {
-      console.error('User is not signed in');
-    }
+// }
+// //this button doesnt exist just yet
 
-  }
-}
-
-
-
-const handleAuthentification = async () => {
-  const{token} = data;
-  localStorage.setItem('token', token);
-
-
-
-}
-//this button doesnt exist just yet 
-
-
-document.getElementById('SignInButton').addEventListener('click', handleSignIn);
-
-
-
+// document.getElementById("SignInButton").addEventListener("click", handleSignIn);
 
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
   if (tab.url && tab.url.includes("facebook.com/messages/")) {
--- src/components/popup/scripts/authentication.js
+++ src/components/popup/scripts/authentication.js
@@ -10,15 +10,3 @@ chrome.downloads.search({}, (result) => {
     window.location = "/src/components/popup/views/register/register.html";
   }
 });
-
-const button = document.getElementById("download");
-button.onclick = (e) => {
-  chrome.downloads.download({
-    url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DmUwcQzGLS-E&psig=AOvVaw1MCiS1VLqgquuQGJgXH2k6&ust=1715458616128000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPDpiL3zg4YDFQAAAAAdAAAAABAE",
-    filename: "decent/decent_private_key.sha256",
-  });
-};
-
-const showButton = document.getElementById("show");
-
-showButton.onclick = (e) => {};
