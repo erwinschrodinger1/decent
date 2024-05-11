f5ad88c9118274276c94f12f7273ffdcebd2f386
erwinschrodinger1 <sunnynirjal@gmail.com>
2024-05-11 12:25:23 +0545

    changes

--- src/components/popup/scripts/popup.js
+++ src/components/popup/scripts/popup.js
@@ -1,5 +1,13 @@
-const popup_button = document
-  .getElementById("popup_button")
-  .addEventListener("click", () => {
-    // alert("Hello WOrd");
-  });
+// const popup_button = document
+//   .getElementById("popup_button")
+//   .addEventListener("click", () => {
+//     // alert("Hello WOrd");
+//   });
+
+const encrupt_button = document.getElementById("updateButton");
+
+encrupt_button.onclick = () => {
+  alert("Something wrong");
+
+  const textbox = document.querySelector(".notranslate");
+};
